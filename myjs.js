2131cbf598b4e6cc56f736732fd06f2d1b9c074c
iamgaelle <85821889+iamgaelle@users.noreply.github.com>
2021-07-31 14:59:13 +0800

    small mistake in footer, insta section padding

--- myjs.js
+++ myjs.js
@@ -1,40 +1,62 @@
-// initialise the hamburger - right option
-$(".hamburger-button").click(function(){
-    $(".mobile-menu").slideToggle();
-    $(this).toggleClass("active");
-  });
 
-  // initialise the hamburger - left option
-$(".hamburger-button-left").click(function(){
-    $(".mobile-menu").slideToggle();
-    $(this).toggleClass("active");
-  });
+
+
+$(document).ready(function() {
+
+
+// initialise the hamburger - right option
+  $(".hamburger-button").click(function(){
+      $(".mobile-menu").slideToggle();
+      $(this).toggleClass("active");
+    });
+
+    // initialise the hamburger - left option
+  $(".hamburger-button-left").click(function(){
+      $(".mobile-menu").slideToggle();
+      $(this).toggleClass("active");
+    });
 
 
 // initialise the script for slick
-$('.slider-container').slick({
-    autoplay: true, // Do we want it to autoplay? true or false
-      autoplaySpeed: 3000, // How long between each slide when auto-playing
-      speed: 1000, // How fast is the transition in milliseconds
-      arrows: true, // Do you want to show arrows to trigger each slide
-      accessibility: true, // Enables keyboard tabbing and arrow key navigation
-      dots: true, // Enables the dots below to show how many slides
-      fade: false, // Changes the animate from slide to fade if true
-      infinite: true, // When true, means that it will scroll in a circle
-      pauseOnHover: true, // When true means the autoplay pauses when hovering
-      pauseOnDotsHover: true, // Pauses the autoplay when hovering over the dots
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    responsive: [
-    //   {
-    //     breakpoint: 768,
-    //     settings: {
-    //       slidesToShow: 1,
-    //       slidesToScroll: 1,
-    //       infinite: true,
-    //       dots: true
-    //     }
-    //   }
-    ]
+  $('.slider-container').slick({
+      autoplay: true, // Do we want it to autoplay? true or false
+        autoplaySpeed: 3000, // How long between each slide when auto-playing
+        speed: 1000, // How fast is the transition in milliseconds
+        arrows: true, // Do you want to show arrows to trigger each slide
+        accessibility: true, // Enables keyboard tabbing and arrow key navigation
+        dots: true, // Enables the dots below to show how many slides
+        fade: false, // Changes the animate from slide to fade if true
+        infinite: true, // When true, means that it will scroll in a circle
+        pauseOnHover: true, // When true means the autoplay pauses when hovering
+        pauseOnDotsHover: true, // Pauses the autoplay when hovering over the dots
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+      //   {
+      //     breakpoint: 768,
+      //     settings: {
+      //       slidesToShow: 1,
+      //       slidesToScroll: 1,
+      //       infinite: true,
+      //       dots: true
+      //     }
+      //   }
+      ]
+    });
+
+// This is the instagram gallery
+      $('[data-fancybox="gallery"]').fancybox({
+        protect: true,
+        keyboard: true,
+        arrows: true,
+        infobar: false,
+        buttons: [
+        "close"
+        ],
+      });
+    
+ 
+    
+
   });
   
