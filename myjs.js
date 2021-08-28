95892d6af1e7305ca1111ca657aa8eabb3bcbaa2
iamgaelle <85821889+iamgaelle@users.noreply.github.com>
2021-08-28 16:50:25 +0800

    hb-menu and product accordion tweeks

--- myjs.js
+++ myjs.js
@@ -16,63 +16,13 @@ $(document).ready(function() {
       $(this).toggleClass("active");
     });
 
-
-// initialise the script for slick
-  $('.slider-container').slick({
-      autoplay: true, // Do we want it to autoplay? true or false
-        autoplaySpeed: 3000, // How long between each slide when auto-playing
-        speed: 1000, // How fast is the transition in milliseconds
-        arrows: false, // Do you want to show arrows to trigger each slide
-        accessibility: true, // Enables keyboard tabbing and arrow key navigation
-        dots: true, // Enables the dots below to show how many slides
-        fade: false, // Changes the animate from slide to fade if true
-        infinite: true, // When true, means that it will scroll in a circle
-        pauseOnHover: true, // When true means the autoplay pauses when hovering
-        pauseOnDotsHover: true, // Pauses the autoplay when hovering over the dots
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        responsive: [
-      //   {
-      //     breakpoint: 768,
-      //     settings: {
-      //       slidesToShow: 1,
-      //       slidesToScroll: 1,
-      //       infinite: true,
-      //       dots: true
-      //     }
-      //   }
-      ]
-    });
-
-    $('.slider-for').slick({
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      arrows: false,
-      fade: true,
-      asNavFor: '.slider-nav'
-    });
-    $('.slider-nav').slick({
-      slidesToShow: 4,
-      slidesToScroll: 4,
-      asNavFor: '.slider-for',
-      dots: false,
-      centerMode: false,
-      focusOnSelect: true
-    });
+   // initialise the hamburger - left option
+   $(".hb-menu-dropdown a").click(function(){
+    $(".mobile-menu").slideToggle();
+    $(this).toggleClass("active");
+  });
 
 
-    
- // This creates a new typing animation in vanilla javascript 
-
- var typed = new Typed('#typed', {
-  stringsElement: '#typed-strings',
-	loop: true,
-  loopCount: 1,
-  // backSpeed: 0,
-  // backDelay: 500,
-	typeSpeed: 100
-});
-    
 // for dropdown descriptions - product pages
 var allPanels = $('.accordion > div').hide();
 
@@ -101,6 +51,21 @@ $('.hb-menu-accordion > span').click(function() {
   
   return false;
 });
+
+
+
+    
+ // This creates a new typing animation in vanilla javascript 
+
+ var typed = new Typed('#typed', {
+  stringsElement: '#typed-strings',
+	loop: true,
+  loopCount: 1,
+  // backSpeed: 0,
+  // backDelay: 500,
+	typeSpeed: 100
+});
+    
 
 // This is the instagram gallery
   $('[data-fancybox="gallery"]').fancybox({
---
+++ slick.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+
+
+
+// initialise the script for slick
+$('.slider-container').slick({
+    autoplay: true, // Do we want it to autoplay? true or false
+      autoplaySpeed: 3000, // How long between each slide when auto-playing
+      speed: 1000, // How fast is the transition in milliseconds
+      arrows: false, // Do you want to show arrows to trigger each slide
+      accessibility: true, // Enables keyboard tabbing and arrow key navigation
+      dots: true, // Enables the dots below to show how many slides
+      fade: false, // Changes the animate from slide to fade if true
+      infinite: true, // When true, means that it will scroll in a circle
+      pauseOnHover: true, // When true means the autoplay pauses when hovering
+      pauseOnDotsHover: true, // Pauses the autoplay when hovering over the dots
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      responsive: [
+    //   {
+    //     breakpoint: 768,
+    //     settings: {
+    //       slidesToShow: 1,
+    //       slidesToScroll: 1,
+    //       infinite: true,
+    //       dots: true
+    //     }
+    //   }
+    ]
+  });
+
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: '.slider-for',
+    dots: false,
+    centerMode: false,
+    focusOnSelect: true
+  });
+
+});
